af567fa83c98b68ef04326d3711dd83a9574d247
helenpree <163835811+helenpree@users.noreply.github.com>
2024-03-20 13:02:24 +0530

    Update minipro.py
    A simple program of biodata using classes and objects.

--- minipro.py
+++ minipro.py
@@ -1,1 +1,21 @@
+class sets():
+  def __init__(self):
+    self.name="preethi"
+    self.age=22
+    self.qualification="BSC.CS"
+    self.location="chennai"
+    self.fathername="chinnaiah"
+    self.mothername="Rani"
+    self.religion="Christian"
+  def display(self):
+    print("BIODATA","Name: ",self.name,"Age: ",
+    self.age,"Qualification: ",
+    self.qualification,"Location: ",
+    self.location,"Fathername: ",
+    self.fathername,"Mothername: ",
+    self.mothername,"Religion: ",
+    self.religion)
+s1=sets()
+s1.display()
+
 
