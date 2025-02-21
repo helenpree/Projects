5931507a5f4da4e70ff285b81d4f83519426215a
helenpree <helenrani2010@gmail.com>
2025-02-21 12:53:52 +0530

    alter

--- fibonacci.py
+++ fibonacci.py
@@ -1,15 +1,21 @@
 # A simple Fibonacci Series:
 #fibonacci series always starts with 0&1 , where each number is the sum of before 2 numbers.
-
-user_input = int(input("enter the number until the series you want: "))
-
-x=0     
-y=1
-print(x)
-print(y)
-
-for i in range(1,user_input+1):
-    a = x+y
-    x = y
-    y = a
-    print("here is your fibonacci series:- ",a)
+#Fibocci series until the user given series:
+u=int(input("enter num: "))
+i=0
+ft=0
+sc=1
+if u==0:
+    print(0)
+elif u==1:
+    print(0,1)
+else:
+    i=2
+    print(ft)
+    print(sc)
+    while i<=u:
+        x=ft+sc
+        ft=sc
+        sc=x
+        print(x)
+        i+=1
